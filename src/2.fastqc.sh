60b09808889b1c9fceb59c3b4a6ea124e1610f7d
Hang Chen <chen_h@outlook.com>
2025-06-06 16:04:55 -0500

    Update 2.fastqc.sh

--- src/2.fastqc.sh
+++ src/2.fastqc.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 [ ! -d ./3.analysis/ ] && mkdir ./3.analysis/
-[ ! -d ./3.analysis/9.plot/ ] && mkdir ./3.analysis/9.plot/
-[ ! -d ./3.analysis/9.plot/1.fastqc/ ] && mkdir ./3.analysis/9.plot/1.fastqc/
+[ ! -d ./3.analysis/9.plots/ ] && mkdir ./3.analysis/9.plots/
+[ ! -d ./3.analysis/9.plots/1.fastqc/ ] && mkdir ./3.analysis/9.plots/1.fastqc/
 
 # install fastqc
 sudo apt-get update -qq && sudo apt-get install fastqc -y
 
 # run fastqc in parallel
 threads=$(nproc)
-fastqc --threads $threads ./2.fastq/*.fastq ./2.fastq/*.fastq.gz --outdir ./3.analysis/9.plot/1.fastqc/
+fastqc --threads $threads ./2.fastq/*.fastq ./2.fastq/*.fastq.gz --outdir ./3.analysis/9.plots/1.fastqc/
 
 # extract per_base_quality.png
-for zip in ./3.analysis/9.plot/1.fastqc/*.zip; do
+for zip in ./3.analysis/9.plots/1.fastqc/*.zip; do
     base=$(basename "$zip" .zip)
-    unzip -p "$zip" "${base}/Images/per_base_quality.png" > "./3.analysis/9.plot/1.fastqc/${base}_per_base_quality.png"
+    unzip -p "$zip" "${base}/Images/per_base_quality.png" > "./3.analysis/9.plots/1.fastqc/${base}_per_base_quality.png"
 done
 
 # cleanup
-rm -f ./3.analysis/9.plot/1.fastqc/*.html ./3.analysis/9.plot/1.fastqc/*.zip
+rm -f ./3.analysis/9.plots/1.fastqc/*.html ./3.analysis/9.plots/1.fastqc/*.zip
