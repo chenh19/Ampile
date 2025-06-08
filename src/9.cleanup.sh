363ed61572f9a37906210fe6861bf31eee0a0920
Hang Chen <chen_h@outlook.com>
2025-06-08 16:51:42 -0500

    Update 9.cleanup.sh

--- src/9.cleanup.sh
+++ src/9.cleanup.sh
@@ -32,3 +32,5 @@ case "$choice" in
         echo -e " \n${TEXT_GREEN}Large intermediate files deleted.${TEXT_RESET} \n" && sleep 1;;
   * )   echo -e " \n${TEXT_YELLOW}Large intermediate files retained.${TEXT_RESET} \n" && sleep 1;;
 esac
+
+echo -e "${TEXT_GREEN}Done.\n\nYou may now proceed to analyze the relative mutation rate.\n${TEXT_RESET} \n"
