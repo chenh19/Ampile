cadcc575ff34393eaf98bf7fe09fe03c2ec081eb
Hang Chen <chen_h@outlook.com>
2025-06-09 17:21:57 -0500

    Update 7.parse.R

--- src/7.parse.R
+++ src/7.parse.R
@@ -32,17 +32,17 @@ message("\nCalculating mutation rate...\n")
 pileup_files <- list.files(path='./3.analysis/4.mpileup', pattern='*.mpileup', full.names = TRUE)
 registerDoParallel(numCores)
 invisible(foreach(pileup_file = pileup_files, .combine = c) %dopar% {
-  
+
   ## initialize
-  mut_profile <- c("Region,Position,Ref_base,Depth,Mut_percentage")
-  
+  mut_profile <- c("Region,Position,Ref_base,Depth,Mut_count,Mut_percentage")
+
   ## read the file
   pileup_data <- readLines(pileup_file)
   filename <- paste0("./3.analysis/8.spreadsheets/3.mpileup_parse/", basename(pileup_file), ".csv")
-  
+
   ## read each line
   for (line in pileup_data) {
-    
+
     ### read base info
     fields <- strsplit(line, "\t")[[1]]
     chr <- fields[1]
@@ -50,7 +50,7 @@ invisible(foreach(pileup_file = pileup_files, .combine = c) %dopar% {
     ref_base <- fields[3]
     depth <- as.numeric(fields[4])
     read_bases <- fields[5]
-    
+
     ### calculate mutation rate
     match <- 0
     for (base in strsplit(read_bases, "")[[1]]) {
@@ -61,13 +61,14 @@ invisible(foreach(pileup_file = pileup_files, .combine = c) %dopar% {
         match <- match + 1
       }
     }
-    mut_percentage <- (depth - match) / depth * 100
-    mut_profile <- c(mut_profile, paste(chr, pos, ref_base, depth, round(mut_percentage, 4), sep = ","))
+    mut_count <- depth - match
+    mut_percentage <- mut_count / depth * 100
+    mut_profile <- c(mut_profile, paste(chr, pos, ref_base, depth, mut_count, round(mut_percentage, 4), sep = ","))
   }
-  
+
   ### write output spreadsheet
   writeLines(mut_profile, filename)
-  
+
   ### notify
   message(paste0("  ", basename(pileup_file), ": mutation rate successfully parsed"))
 })
