89a1bbf1e799ee3bf155d98bed5301f70f5e6231
Hang Chen <chen_h@outlook.com>
2025-06-18 13:59:04 -0500

    Update 9.cleanup.sh

--- src/9.cleanup.sh
+++ src/9.cleanup.sh
@@ -8,23 +8,17 @@ TEXT_RESET=$(tput sgr0)
 # cleanup
  read -n1 -s -r -p $'\n'"$(echo -e $TEXT_YELLOW'Would you like delete large intermediate files? [y/n/c]'$TEXT_RESET)"$' \n' choice
 case "$choice" in
-  y|Y ) # trim
-        if find "./3.analysis/2.trim/" -maxdepth 1 -type f -name "*.trimmed.fastq.gz" | grep -q .; then
-          rm -f ./3.analysis/2.trim/*.trimmed.fastq.gz
-          echo -e "" > ./3.analysis/2.trim/large_intermediate_files_deleted.txt
-        fi
+  y|Y ) # refseq
+        [ -d ./3.analysis/1.refseq/ ] && rm -rf ./3.analysis/1.refseq/
+  
+        # trim
+        [ -d ./3.analysis/2.trim/ ] && rm -rf ./3.analysis/2.trim/
 
         # bam
-        if find "./3.analysis/3.bam/" -maxdepth 1 -type f -name "*.filtered.bam" | grep -q .; then
-          rm -f ./3.analysis/3.bam/*.filtered.bam ./3.analysis/3.bam/*.filtered.bam.bai
-          echo -e "" > ./3.analysis/3.bam/large_intermediate_files_deleted.txt
-        fi
+        [ -d ./3.analysis/3.bam/ ] && rm -rf ./3.analysis/3.bam/
 
         # mpileup
-        if find "./3.analysis/4.mpileup/" -maxdepth 1 -type f -name "*.mpileup" | grep -q .; then
-          rm -f ./3.analysis/4.mpileup/*.mpileup
-          echo -e "" > ./3.analysis/4.mpileup/large_intermediate_files_deleted.txt
-        fi
+        [ -d ./3.analysis/4.mpileup/ ] && rm -rf ./3.analysis/4.mpileup/
 
         # Rhistory
         [ -f ./.Rhistory ] && rm -f ./.Rhistory
