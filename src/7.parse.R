6c22de617266dc069c8eec51f442654fd60b179e
Hang Chen <chen_h@outlook.com>
2025-06-19 12:56:43 -0500

    Update 7.parse.R

--- src/7.parse.R
+++ src/7.parse.R
@@ -7,7 +7,7 @@ TEXT_RESET  <- "\033[0m"
 cat("\n", TEXT_YELLOW, "Parsing pileup files...", TEXT_RESET, "\n\n", sep = "")
 
 # load packages
-packages <- c("filesstrings", "foreach", "doParallel")
+packages <- c("filesstrings", "doParallel", "foreach")
 for (package in packages) {
   if (!suppressPackageStartupMessages(require(package, character.only = TRUE))) {
     install.packages(package, repos = "https://cloud.r-project.org")
