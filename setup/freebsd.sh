505bfb76ee69de0844763aebb7c1f88751224358
Hang Chen <chen_h@outlook.com>
2025-06-19 10:16:01 -0500

    Update freebsd.sh

--- setup/freebsd.sh
+++ setup/freebsd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # for freebsd and derivatives
 sudo pkg install -y R bwa fastqc fastp samtools bamtools
-sudo Rscript -e "install.packages(c('tidyverse', 'ggplot2', 'expss', 'filesstrings', 'foreach', 'doParallel'), force = TRUE, repos = 'https://cloud.r-project.org')"
+sudo Rscript -e "install.packages(c('dplyr', 'tidyr', 'ggplot2', 'expss', 'filesstrings', 'foreach', 'doParallel'), force = TRUE, repos = 'https://cloud.r-project.org')"
