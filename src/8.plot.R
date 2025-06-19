64748a7e53b653b2b5ae633e858dfdd794b33137
Hang Chen <chen_h@outlook.com>
2025-06-19 10:10:50 -0500

    Update 8.plot.R

--- src/8.plot.R
+++ src/8.plot.R
@@ -7,7 +7,7 @@ TEXT_RESET  <- "\033[0m"
 cat("\n", TEXT_YELLOW, "Generating plots...", TEXT_RESET, "\n\n", sep = "")
 
 # load packages
-packages <- c("tidyverse", "ggplot2", "expss", "filesstrings")
+packages <- c("dplyr", "tidyr", "ggplot2", "expss", "filesstrings")
 for (package in packages) {
   if (!suppressPackageStartupMessages(require(package, character.only = TRUE))) {
     install.packages(package, repos = "https://cloud.r-project.org", quiet = TRUE)
