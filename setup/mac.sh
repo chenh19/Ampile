0761a05fdba1cd4f5fd7ba0661d76adc78923d1a
Hang Chen <chen_h@outlook.com>
2025-06-19 10:17:12 -0500

    Update mac.sh

--- setup/mac.sh
+++ setup/mac.sh
@@ -2,4 +2,4 @@
 # for mac
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install r bwa fastqc fastp samtools bamtools
-Rscript -e "install.packages(c('tidyverse', 'ggplot2', 'expss', 'filesstrings', 'foreach', 'doParallel'), force = TRUE, repos = 'https://cloud.r-project.org')"
+Rscript -e "install.packages(c('dplyr', 'tidyr', 'ggplot2', 'expss', 'filesstrings', 'foreach', 'doParallel'), force = TRUE, repos = 'https://cloud.r-project.org')"
