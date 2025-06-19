5c0a981d29f2e148df85938347840c8878adc281
Hang Chen <chen_h@outlook.com>
2025-06-19 11:57:19 -0500

    Update 6.mpileup.sh

--- src/6.mpileup.sh
+++ src/6.mpileup.sh
@@ -13,10 +13,11 @@ echo -e "\n${TEXT_YELLOW}Generating pileup files...${TEXT_RESET}\n" && sleep 1
 [ ! -d ./3.analysis/4.mpileup/ ] && mkdir ./3.analysis/4.mpileup/
 
 # pileup
-for bam in ./3.analysis/3.bam/*.filtered.bam; do
+ls ./3.analysis/3.bam/*.filtered.bam | parallel '
+  bam={}
   base=$(basename "$bam" .filtered.bam)
   samtools mpileup -aa -A -B -Q 0 -d 2000000 -f ./3.analysis/1.refseq/refseq.fa "$bam" > ./3.analysis/4.mpileup/"$base".mpileup
-done
+'
 
 # notify end
 echo -e "\n${TEXT_GREEN}Done.${TEXT_RESET}\n" && sleep 1
