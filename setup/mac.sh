f5d4201bc6ba311a4d53d86159e85ec305637994
Hang Chen <chen_h@outlook.com>
2025-06-19 08:59:02 -0500

    Create mac.sh

---
+++ setup/mac.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# for mac
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install r bwa fastqc fastp samtools bamtools
+Rscript -e "install.packages(c('tidyverse', 'ggplot2', 'expss', 'filesstrings', 'foreach', 'doParallel'), force = TRUE, repos = 'https://cloud.r-project.org')"
