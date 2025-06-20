48cf0c0a1e8d38f6bff40a5b03521b8772f2d9ab
Hang Chen <chen_h@outlook.com>
2025-06-20 17:10:20 -0500

    Update 1.refseq.sh

--- src/1.refseq.sh
+++ src/1.refseq.sh
@@ -9,8 +9,7 @@ TEXT_RESET="$(tput sgr0)"
 echo -e "\n${TEXT_YELLOW}Processing reference sequences...${TEXT_RESET}\n" && sleep 1
 
 # create folders
-[ ! -d ./3.analysis/ ] && mkdir ./3.analysis/
-[ ! -d ./3.analysis/1.refseq/ ] && mkdir ./3.analysis/1.refseq/
+mkdir -p ./3.analysis/1.refseq/
 
 # combine and index refseq
 cat ./1.ref/*.fa > ./3.analysis/1.refseq/refseq.fa
