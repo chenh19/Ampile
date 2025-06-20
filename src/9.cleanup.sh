b377a593d272ad3cee4316ffc7b4e6b0f48fecba
Hang Chen <chen_h@outlook.com>
2025-06-20 16:32:12 -0500

    Update 9.cleanup.sh

--- src/9.cleanup.sh
+++ src/9.cleanup.sh
@@ -6,7 +6,7 @@ TEXT_GREEN="$(tput bold)$(tput setaf 2)"
 TEXT_RESET="$(tput sgr0)"
 
 # cleanup
-read -n1 -s -r -p $'\n'"$(echo -e $TEXT_YELLOW'Would you like delete large intermediate files? [y/n/c]'$TEXT_RESET)"$' \n' choice
+read -n1 -s -r -p $'\n'"$(echo -e $TEXT_YELLOW'Would you like delete large intermediate files? [y/n/c]'$TEXT_RESET)"$'\n' choice
 case "$choice" in
   y|Y ) ## refseq
         [ -d ./3.analysis/1.refseq/ ] && rm -rf ./3.analysis/1.refseq/
@@ -23,9 +23,9 @@ case "$choice" in
         ## Rhistory
         [ -f ./.Rhistory ] && rm -f ./.Rhistory
 
-        echo -e " \n${TEXT_GREEN}Large intermediate files deleted.${TEXT_RESET} \n" && sleep 1;;
-  * )   echo -e " \n${TEXT_YELLOW}Large intermediate files retained.${TEXT_RESET} \n" && sleep 1;;
+        echo -e "\n${TEXT_GREEN}Large intermediate files deleted.${TEXT_RESET}\n" && sleep 1;;
+  * )   echo -e "\n${TEXT_YELLOW}Large intermediate files retained.${TEXT_RESET}\n" && sleep 1;;
 esac
 
 # notify
-echo -e "\n${TEXT_GREEN}All done! You may now proceed to analyze the relative mutation rate.\n${TEXT_RESET} \n"
+echo -e "\n${TEXT_GREEN}All done! You may now proceed to analyze the relative mutation rate.\n${TEXT_RESET}\n"
