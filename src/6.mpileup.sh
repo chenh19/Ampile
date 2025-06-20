5ac149d3648d288c169fd27a2684ce60220e9ac7
Hang Chen <chen_h@outlook.com>
2025-06-20 17:47:53 -0500

    Update 6.mpileup.sh

--- src/6.mpileup.sh
+++ src/6.mpileup.sh
@@ -9,13 +9,22 @@ TEXT_RESET="$(tput sgr0)"
 echo -e "\n${TEXT_YELLOW}Generating pileup files...${TEXT_RESET}\n" && sleep 1
 
 # create folders
-[ ! -d ./3.analysis/ ] && mkdir ./3.analysis/
-[ ! -d ./3.analysis/4.mpileup/ ] && mkdir ./3.analysis/4.mpileup/
-[ ! -d ~/.parallel/ ] && mkdir ~/.parallel/
+mkdir -p ./3.analysis/4.mpileup/
+mkdir -p ~/.parallel/
 [ ! -f ~/.parallel/will-cite ] && touch ~/.parallel/will-cite
 
+# set threads for parallel processing
+if command -v nproc >/dev/null 2>&1; then
+  threads=$(nproc)
+else
+  threads=$(sysctl -n hw.ncpu)
+fi
+if [ "$threads" -gt 32 ]; then
+  threads=32
+fi
+
 # pileup
-ls ./3.analysis/3.bam/*.filtered.bam | parallel '
+ls ./3.analysis/3.bam/*.filtered.bam | parallel -j $threads '
   bam={}
   base=$(basename "$bam" .filtered.bam)
   samtools mpileup -aa -A -B -Q 0 -d 2000000 -f ./3.analysis/1.refseq/refseq.fa "$bam" > ./3.analysis/4.mpileup/"$base".mpileup
