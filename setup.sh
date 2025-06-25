78a96e556415b27edef9f7f30ca5fe25946edccf
Hang Chen <chen_h@outlook.com>
2025-06-24 19:43:26 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,24 @@ echo -e "\n${TEXT_YELLOW}Setting up environment for Ampile pipeline...${TEXT_RES
 mkdir -p ~/.parallel/
 [ ! -f ~/.parallel/will-cite ] && touch ~/.parallel/will-cite
 
+# check installed package function
+check_installed_packages() {
+    TEXT_GREEN="$(tput bold)$(tput setaf 2)"
+    TEXT_RESET="$(tput sgr0)"
+    printf "\nChecking packages:\n\n"
+    required_tools="R bwa fastqc fastp samtools bamtools parallel"
+    for tool in $required_tools; do
+        if command -v "$tool" >/dev/null 2>&1; then
+            printf "  - Successfully installed: %s\n\n" "$tool"
+        else
+            printf "  x Failed to install: %s\n\n" "$tool"
+        fi
+    done
+    Rscript -e 'for (pkg in c("tidyverse", "expss", "filesstrings", "foreach", "doParallel")) if (suppressWarnings(suppressPackageStartupMessages(require(pkg, character.only = TRUE)))) message("  - Successfully installed: r-", pkg, "\n") else message("  x Failed to install: r-", pkg, "\n")'
+    printf "\n%sEnvironment setup complete! You may now proceed to run the Ampile pipeline.%s\n\n\n" "$TEXT_GREEN" "$TEXT_RESET"
+    sleep 1
+}
+
 # check OS
 case "$(uname -s)" in
     Linux)
@@ -126,22 +144,6 @@ R CMD javareconf
 conda update --all -y
 
 # check installed packages
-check_installed_packages() {
-    TEXT_GREEN="$(tput bold)$(tput setaf 2)"
-    TEXT_RESET="$(tput sgr0)"
-    printf "\nChecking packages:\n\n"
-    required_tools="R bwa fastqc fastp samtools bamtools parallel"
-    for tool in $required_tools; do
-        if command -v "$tool" >/dev/null 2>&1; then
-            printf "  - Successfully installed: %s\n\n" "$tool"
-        else
-            printf "  x Failed to install: %s\n\n" "$tool"
-        fi
-    done
-    Rscript -e 'for (pkg in c("tidyverse", "expss", "filesstrings", "foreach", "doParallel")) if (suppressWarnings(suppressPackageStartupMessages(require(pkg, character.only = TRUE)))) message("  - Successfully installed: r-", pkg, "\n") else message("  x Failed to install: r-", pkg, "\n")'
-    printf "\n%sEnvironment setup complete! You may now proceed to run the Ampile pipeline.%s\n\n\n" "$TEXT_GREEN" "$TEXT_RESET"
-    sleep 1
-}
 check_installed_packages
 
 # deactivate ampile and base
