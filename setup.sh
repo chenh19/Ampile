441b1bd2efc2e54bfa1a3df5773c8424c494ef16
Hang Chen <chen_h@outlook.com>
2025-06-26 17:09:12 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,13 +18,14 @@ check_installed_packages() {
     TEXT_RESET="$(tput sgr0)"
     printf "\nChecking packages:\n\n"
     required_tools=("R" "bwa" "fastqc" "fastp" "samtools" "bamtools" "parallel")
-    for tool in $required_tools; do
+    for tool in "${required_tools[@]}"; do
         if command -v "$tool" >/dev/null 2>&1; then
             printf "  - Successfully installed: %s\n\n" "$tool"
         else
             printf "  x Failed to install: %s\n\n" "$tool"
         fi
     done
+
     Rscript -e 'for (pkg in c("tidyverse", "expss", "filesstrings", "foreach", "doParallel")) if (suppressWarnings(suppressPackageStartupMessages(require(pkg, character.only = TRUE)))) message("  - Successfully installed: r-", pkg, "\n") else message("  x Failed to install: r-", pkg, "\n")'
     printf "\n%sEnvironment setup complete! You may now proceed to run the Ampile pipeline.%s\n\n\n" "$TEXT_GREEN" "$TEXT_RESET"
     sleep 1
