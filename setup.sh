bdb32bedc6d65846e8cc3285421be04b2c938085
Hang Chen <chen_h@outlook.com>
2025-06-26 17:37:08 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,25 +12,6 @@ echo -e "\n${TEXT_YELLOW}Setting up environment for Ampile pipeline...${TEXT_RES
 mkdir -p ~/.parallel/
 [ ! -f ~/.parallel/will-cite ] && touch ~/.parallel/will-cite
 
-# check installed package function
-check_installed_packages() {
-    TEXT_GREEN="$(tput bold)$(tput setaf 2)"
-    TEXT_RESET="$(tput sgr0)"
-    printf "\nChecking packages:\n\n"
-    required_tools=("R" "bwa" "fastqc" "fastp" "samtools" "bamtools" "parallel")
-    for tool in "${required_tools[@]}"; do
-        if command -v "$tool" >/dev/null 2>&1; then
-            printf "  - Successfully installed: %s\n\n" "$tool"
-        else
-            printf "  x Failed to install: %s\n\n" "$tool"
-        fi
-    done
-
-    Rscript -e 'for (pkg in c("tidyverse", "expss", "filesstrings", "foreach", "doParallel")) if (suppressWarnings(suppressPackageStartupMessages(require(pkg, character.only = TRUE)))) message("  - Successfully installed: r-", pkg, "\n") else message("  x Failed to install: r-", pkg, "\n")'
-    printf "\n%sEnvironment setup complete! You may now proceed to run the Ampile pipeline.%s\n\n\n" "$TEXT_GREEN" "$TEXT_RESET"
-    sleep 1
-}
-
 # check OS
 case "$(uname -s)" in
     Linux)
@@ -44,10 +25,16 @@ case "$(uname -s)" in
         fi
         ;;
     Darwin)
+        [ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+        [ ! -f ~/.zprofile ] && touch ~/.zprofile
         if [[ "$(uname -m)" == "x86_64" ]]; then
             URL="https://cran.r-project.org/bin/macosx/big-sur-x86_64/base/R-4.5.1-x86_64.pkg"
+            if ! grep -q 'eval "$(/usr/local/bin/brew shellenv)"' ~/.bash_profile ; then echo 'eval "$(/usr/local/bin/brew shellenv)"' >> ~/.bash_profile; fi
+	        if ! grep -q 'eval "$(/usr/local/bin/brew shellenv)"' ~/.zprofile ; then echo 'eval "$(/usr/local/bin/brew shellenv)"' >> ~/.zprofile; fi
         elif [[ "$(uname -m)" == "arm64" ]]; then
             URL="https://cran.r-project.org/bin/macosx/big-sur-arm64/base/R-4.5.1-arm64.pkg"
+            if ! grep -q 'eval "$(/opt/homebrew/bin/brew shellenv)"' ~/.bash_profile ; then echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bash_profile; fi
+	        if ! grep -q 'eval "$(/opt/homebrew/bin/brew shellenv)"' ~/.zprofile ; then echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile; fi
             /usr/sbin/softwareupdate --install-rosetta --agree-to-license
         else
             echo -e "\n${TEXT_YELLOW}Unsupported MacOS architecture: $(uname -m)${TEXT_RESET}\n" >&2
@@ -57,22 +44,20 @@ case "$(uname -s)" in
         printf "\n\n\033[1mInstall the Xcode Command Line Tools if a popup appears, then proceed.\033[0m\n\n"
         printf "\n\033[1mInitializing Homebrew setup...\033[0m\n\n"
         bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        echo 'eval "$($(which brew) shellenv)"' >> ~/.bash_profile
-        echo 'eval "$($(which brew) shellenv)"' >> ~/.zprofile
         eval "$($(which brew) shellenv)"
         brew install bwa fastqc fastp samtools bamtools parallel
         curl -fsSL "$URL" -o ~/R.pkg
         sudo installer -pkg ~/R.pkg -target /
         rm -f ~/R.pkg
         Rscript -e "install.packages(c('tidyverse', 'expss', 'filesstrings', 'foreach', 'doParallel'), force = TRUE, repos = 'https://cloud.r-project.org')"
-        check_installed_packages
+        bash -c "$(curl -fsSL https://raw.githubusercontent.com/chenh19/Ampile/refs/heads/main/check.sh)"
         exit 0
         ;;
     FreeBSD)
         if grep -q '^ID=freebsd' /etc/os-release 2>/dev/null; then
             sudo pkg upgrade -y && sudo pkg install -y R bwa fastqc fastp samtools bamtools parallel R-cran-tidyverse R-cran-foreach R-cran-doParallel zip
             sudo Rscript -e "install.packages(c('expss', 'filesstrings'), force = TRUE, repos = 'https://cloud.r-project.org')"
-            check_installed_packages
+            bash -c "$(curl -fsSL https://raw.githubusercontent.com/chenh19/Ampile/refs/heads/main/check.sh)"
             exit 0
         else
             OS_NAME=$(grep '^NAME=' /etc/os-release 2>/dev/null | cut -d= -f2- | tr -d '"')
@@ -148,7 +133,7 @@ R CMD javareconf
 conda update --all -y
 
 # check installed packages
-check_installed_packages
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/chenh19/Ampile/refs/heads/main/check.sh)"
 
 # deactivate ampile and base
 conda deactivate
