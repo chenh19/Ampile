227512ba4646bc8726d82ea60cd5529291a41514
Hang Chen <chen_h@outlook.com>
2025-06-26 20:15:38 -0500

    Update check.sh

--- check.sh
+++ check.sh
@@ -4,6 +4,7 @@
 [ -d ~/miniconda3/envs/ampile/ ] && source ~/miniconda3/etc/profile.d/conda.sh && conda activate ampile
 error=0
 check_installed_packages() {
+    TEXT_YELLOW="$(tput bold)$(tput setaf 3)"
     TEXT_GREEN="$(tput bold)$(tput setaf 2)"
     TEXT_RESET="$(tput sgr0)"
     printf "\nChecking packages:\n\n"
@@ -16,20 +17,12 @@ check_installed_packages() {
             error=1
         fi
     done
-    output=$(Rscript -e 'for (pkg in c("tidyverse", "expss", "filesstrings", "foreach", "doParallel")) if (!suppressWarnings(suppressPackageStartupMessages(require(pkg, character.only = TRUE)))) cat("\033[1;33m", "Error: r-", pkg, " is not installed.", "\033[0m", "\n\n", sep = "")' 2>/dev/null)
-    if echo "$output" | grep -q "Error:"; then
-      echo "  $output" && echo ""
-      error=1
-    fi
+    output=$(Rscript -e 'pkgs <- c("tidyverse", "expss", "filesstrings", "foreach", "doParallel"); failed <- FALSE; for (pkg in pkgs) { if (suppressWarnings(suppressPackageStartupMessages(require(pkg, character.only = TRUE)))) { message("  - Successfully installed: r-", pkg, "\n") } else { message("  x Failed to install: r-", pkg, "\n"); failed <- TRUE } }; if (failed) quit(status=1)'); r_exit=$?; [[ $r_exit -ne 0 ]] && error=1
     if (( error )); then
-      echo -e "${TEXT_YELLOW}Please setup the workspace and try again.${TEXT_RESET}\n" >&2 && sleep 1
-      exit 1
+      printf "\n%sEnvironment not ready. Please set up the environment and try again.%s\n\n\n" "$TEXT_YELLOW" "$TEXT_RESET" && sleep 1
     else
-      echo -e "${TEXT_GREEN}Done.${TEXT_RESET}\n" && sleep 1
+      printf "\n%sEnvironment setup complete! You may now proceed to run the Ampile pipeline.%s\n\n\n" "$TEXT_GREEN" "$TEXT_RESET" && sleep 1
     fi
-    #Rscript -e 'for (pkg in c("tidyverse", "expss", "filesstrings", "foreach", "doParallel")) if (suppressWarnings(suppressPackageStartupMessages(require(pkg, character.only = TRUE)))) message("  - Successfully installed: r-", pkg, "\n") else message("  x Failed to install: r-", pkg, "\n")'
-    #printf "\n%sEnvironment setup complete! You may now proceed to run the Ampile pipeline.%s\n\n\n" "$TEXT_GREEN" "$TEXT_RESET"
-    #sleep 1
 }
 check_installed_packages
 [ -d ~/miniconda3/envs/ampile/ ] && conda deactivate
